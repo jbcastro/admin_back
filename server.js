433c318bccf648234c9782828bd41977d2366fc8
jbcastro <jbeckcastro810@southseattle.edu>
2020-03-10 19:01:33 -0700

    aws 3?

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 var cors = require("cors");
 
-const path = require("path");
-
 const app = express();
 app.use(cors());
-const port =
-  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 5000;
+const port = process.env.PORT || 5000;
 var wineMethods = require("./models/wineMethods");
 var Wines = require("./models/wine");
 const bodyParser = require("body-parser");
@@ -14,17 +11,15 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.engine("html", require("ejs").renderFile);
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "./client/public"));
-app.use(express.static(path.join(__dirname, "./client/public")));
+// console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get("/", function(req, res) {
-  res.render("index", {});
-});
-app.get("/api", (req, res, next) => {
+// create a GET route
+// app.get('/', (req, res) => {
+//   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+// });
+
+app.get("/", (req, res, next) => {
   wineMethods
     .getAll()
     .then(items => {
@@ -36,7 +31,7 @@ app.get("/api", (req, res, next) => {
     });
 });
 
-app.get("/api/get/", (req, res, next) => {
+app.get("/get", (req, res, next) => {
   wineMethods
     .getOne(req.query._id)
     .then(items => {
@@ -47,7 +42,7 @@ app.get("/api/get/", (req, res, next) => {
     });
 });
 
-app.get("/api/delete", (req, res, next) => {
+app.get("/delete", (req, res, next) => {
   wineMethods
     .killOne(req.query._id)
     .then(items => {
@@ -58,99 +53,79 @@ app.get("/api/delete", (req, res, next) => {
     });
 });
 
-app.put("/api/add", (req, res, next) => {
-  Wines.updateOne(
-    {
-      _id: req.body._id
-    },
-    {
-      vinyard: req.body.vinyard,
+app.post("/add", (req, res) => {
+  // const
+  if (!req.body._id) {
+    let small = new Wines({
+      _id: req.body._id,
+      // winenum: req.body.winenum,
       name: req.body.name,
-      grape: req.body.grape,
-
+      grape1: req.body.grape1,
+      grape2: req.body.grape2,
+      grape3: req.body.grape3,
       grapes: req.body.grapes,
       year: req.body.year,
       place: req.body.place,
       area: req.body.area,
       country: req.body.country,
       appellation: req.body.appellation,
-      description: req.body.description,
-
+      description1: req.body.description1,
+      description2: req.body.description2,
+      description3: req.body.description3,
+      description4: req.body.description4,
+      description5: req.body.description5,
+      description6: req.body.description6,
+      description7: req.body.description7,
+      description8: req.body.description8,
+      description9: req.body.description9,
+      description10: req.body.description10,
       funfact: req.body.funfact,
+      timestamp: Date.now(),
       price: req.body.price,
-      lastUpdated: Date.now(),
       mise: req.body.mise,
 
       color: req.body.color,
-      status: req.body.status,
-      picture: req.body.picture,
-      coravin: req.body.coravin
-    },
-    (err, result) => {
-      if (err) return next(err);
-      res.json({ updated: result.nModified, _id: req.body._id });
-    }
-  );
-});
-
-app.post("/api/add", (req, res, next) => {
-  if (!req.body._id) {
-    let wine = new Wines(
-      // {
-      //   _id: req.body._id
-      // },
-      {
-        vinyard: req.body.vinyard,
-        name: req.body.name,
-        grape: req.body.grape,
-        grapes: req.body.grapes,
-        year: req.body.year,
-        place: req.body.place,
-        area: req.body.area,
-        country: req.body.country,
-        appellation: req.body.appellation,
-        description: req.body.description,
-        funfact: req.body.funfact,
-        timestamp: Date.now(),
-        price: req.body.price,
-        mise: req.body.mise,
-        lastUpdated: Date.now(),
-
-        color: req.body.color,
-        status: req.body.status,
-        picture: req.body.picture,
-        coravin: req.body.coravin
-      }
-    );
-    // wine.save().then(item => res.json(item));
-    wine.save((err, newWine) => {
-      if (err) return next(err);
-      return res.json({ updated: 0, _id: newWine._id });
+      status: req.body.status
+    });
+    small.save((err, newWine) => {
+      if (err) return handleError(err);
+      res.json({ updated: 0, _id: newWine._id });
     });
   } else {
     Wines.updateOne(
-      { _id: req.body._id },
       {
-        vinyard: req.body.vinyard,
+        _id: req.body._id
+      },
+      {
+        // winenum: req.body.winenum,
         name: req.body.name,
-        grape: req.body.grape,
+        grape1: req.body.grape1,
+        grape2: req.body.grape2,
+        grape3: req.body.grape3,
         grapes: req.body.grapes,
         year: req.body.year,
         place: req.body.place,
         area: req.body.area,
         country: req.body.country,
         appellation: req.body.appellation,
-        description: req.body.description,
+        description1: req.body.description1,
+        description2: req.body.description2,
+        description3: req.body.description3,
+        description4: req.body.description4,
+        description5: req.body.description5,
+        description6: req.body.description6,
+        description7: req.body.description7,
+        description8: req.body.description8,
+        description9: req.body.description9,
+        description10: req.body.description10,
+
         funfact: req.body.funfact,
-        timestamp: Date.now(),
         price: req.body.price,
+        timestamp: Date.now(),
         mise: req.body.mise,
-        lastUpdated: Date.now(),
 
         color: req.body.color,
-        status: req.body.status,
-        picture: req.body.picture,
-        coravin: req.body.coravin
+        status: req.body.status
       },
       (err, result) => {
         if (err) return next(err);
